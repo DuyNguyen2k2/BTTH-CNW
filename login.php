78ae57f058c5996f3d43e07e6044658e0e01f5fb
Khoa <khoadang3211@gmail.com>
2023-02-27 02:08:18 +0700

    check session

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
+    session_start();
+    if(isset($_SESSION['check'])){
+        header("Location:admin/index.php");
+    }
+?>
+<?php
     include './include/header.php';
 ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
